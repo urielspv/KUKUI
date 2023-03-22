8a5a310808e88d5accb77da73b139962cd01ba13
oscarkann <oscarkang97@gmail.com>
2023-03-22 17:37:12 -0600

    MySQL y Footer update, JS Update

--- js/calendario.js
+++ js/calendario.js
@@ -1,19 +1,24 @@
-const getDatePickerTitle = elem => {
-    // From the label or the aria-label
-    const label = elem.nextElementSibling;
-    let titleText = '';
-    if (label && label.tagName === 'LABEL') {
-        titleText = label.textContent;
-    } else {
-        titleText = elem.getAttribute('aria-label') || '';
-    }
-    return titleText;
-}
+const cc_number_input = document.getElementById("cc_number");
+const validate_button = document.getElementById("validate");
+const error_message = document.getElementById("error_message");
 
-const elems = document.querySelectorAll('.datepicker_input');
-for (const elem of elems) {
-    const datepicker = new Datepicker(elem, {
-        'format': 'dd/mm/yyyy', // UK format
-        title: getDatePickerTitle(elem)
-    });
-}
+validate_button.addEventListener("click", function () {
+    const cc_number = cc_number_input.value;
+    if (/^[0-9]{13,16}$/.test(cc_number)) {
+        // El número de tarjeta de crédito es válido
+        error_message.innerText = "";
+    } else {
+        // El número de tarjeta de crédito no es válido
+        error_message.innerText = "Por favor ingrese un número de tarjeta de crédito válido.";
+    }
+});
+
+var card = new Card({
+    form: 'form', // identificador del formulario
+    container: '.card-wrapper', // selector CSS del contenedor de la tarjeta
+    formatting: true, // activa la formateo automático de los números de tarjeta
+    messages: {
+        validDate: 'valid\ndate', // mensaje para fecha de caducidad válida
+        monthYear: 'mm/yy', // formato de fecha de caducidad
+    }
+});
